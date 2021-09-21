17e6c9cda525fbfff06162555fdbf88142feca71
Steven Indrayana <steven.indrayana.istimur@gmail.com>
2021-09-22 01:21:04 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import datetime
+
+content = ''
+
+
+#extracting Hacker News Stories
+
+
+def extract_news(url):
+    print('Extracting Hacker News Stories...')
+    cnt = ''
+    cnt +=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+tag.text + "\n" + '<br>') if tag.text!='More' else '')
+        #print(tag.prettify) #find_all('span',attrs={'class':'sitestr'}))
+    return(cnt)
+    
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>------<br>')
+content +=('<br><br>End of Message')
+
+
+print(content)
